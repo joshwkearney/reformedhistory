543fc1d2c1227831707e73bfca02845f4346a7ae
Barret Glass <bmg7@rice.edu>
2024-06-22 11:24:17 -0500

    failing to @apply

--- src/ReformedHistory/tailwind.config.js
+++ src/ReformedHistory/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
 }
 
