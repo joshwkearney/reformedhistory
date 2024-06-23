d355e46b9ba11e013de4df6bb86b35fdcc09a8c2
Barret Glass <bmg7@rice.edu>
2024-06-22 19:42:08 -0500

    A kind of wacky zoom

--- src/ReformedHistory/Models/TimelineEventModel.cs
+++ src/ReformedHistory/Models/TimelineEventModel.cs
@@ -6,4 +6,10 @@ public record TimelineEventModel {
     public required string Date { get; init; }
     
     public required string Description { get; init; }
+
+
+    // TODO: create a common data structure for documents that we can store here too
+    public required string Category { get; init; }
+
+    public required string DocumentId { get; init; }
 }
