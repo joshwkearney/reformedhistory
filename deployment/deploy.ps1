26f0c16760094c613d04cfa4c486e83f10b778a7
Joshua Kearney <joshwkearney@icloud.com>
2024-09-01 00:23:09 -0500

    Update deploy.ps1

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,22 +1,26 @@
+$stagingDir = "/root/reformedhistory/staging"
+$deployDir = "/root/reformedhistory/deploy"
+$serviceName = "reformedhistory.service"
+
 # Don't do anything if the staging folder is empty
-if (-not (Get-ChildItem -path "/root/reformedhistory/staging/" -Recurse | Select-Object -First 1)) {
+if (-not (Get-ChildItem -path $stagingDir -Recurse | Select-Object -First 1)) {
         Write-Output "Staging directory is empty, skipping deployment"
         return
 }
 
 # Stop the website
-Invoke-Expression "sudo systemctl stop reformedhistory.service"
+Invoke-Expression "sudo systemctl stop ${serviceName}"
 
 # Delete everything in the deployment directory
-Remove-Item -Path "/root/reformedhistory/deploy/*" -Recurse -Force
+Remove-Item -Path "${deployDir}/*" -Recurse -Force
 
 # Copy everything from staging to deployment
-Copy-Item -Path "/root/reformedhistory/staging/*" -Destination "/root/reformedhistory/deploy/" -Recurse -Force
+Copy-Item -Path "${stagingDir}/*" -Destination $deployDir -Recurse -Force
 
 # Delete everything in the staging directory to make space for the next action 
-Remove-Item -Path "/root/reformedhistory/staging/*" -Recurse -Force
+Remove-Item -Path "${stagingDir}/*" -Recurse -Force
 
 # Start the website again
-Invoke-Expression "sudo systemctl start reformedhistory.service"
+Invoke-Expression "sudo systemctl start ${serviceName}"
 
 Write-Output "Deployment complete"
