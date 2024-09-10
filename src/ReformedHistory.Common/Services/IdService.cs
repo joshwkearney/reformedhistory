b04cd7e60ff10a88a101d655a02759abed13d344
Joshua Kearney <joshwkearney@icloud.com>
2024-09-09 23:28:51 -0500

    Fix conflicting id issue

--- src/ReformedHistory.Common/Services/IdService.cs
+++ src/ReformedHistory.Common/Services/IdService.cs
@@ -1,9 +1,9 @@
 namespace ReformedHistory.Common.Services;
 
 public class IdService {
-    private int counter = 0;
+    private static readonly Random Random = new Random();
 
     public string GetUniqueId() {
-        return "Component" + this.counter++;
+        return "Component" + Random.NextInt64();
     }
 }
