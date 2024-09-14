6281ed674d49e4f63404fa42742da008bcfeba2d
Joshua Kearney <joshwkearney@icloud.com>
2024-09-14 15:01:03 -0500

    Add mobile hamburger menu

--- src/ReformedHistory.Common/Services/IdService.cs
+++ src/ReformedHistory.Common/Services/IdService.cs
@@ -1,9 +1,7 @@
 namespace ReformedHistory.Common.Services;
 
 public class IdService {
-    private static readonly Random Random = new Random();
-
     public string GetUniqueId() {
-        return "Component" + Random.NextInt64();
+        return "Component" + Random.Shared.NextInt64();
     }
 }
